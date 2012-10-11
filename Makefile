a550ac1ebbda824e4c935414b1742bb055fc00af
r-plus <anasthasia.r@gmail.com>
2012-10-11 23:46:39 +0900

    v0.7 - Apply Apache License 2.0; Slider setting;

--- Makefile
+++ Makefile
@@ -12,3 +12,15 @@ Toggle_INSTALL_PATH = /var/mobile/Library/SBSettings/Toggles/FakeClockUp
 Toggle_OBJC_FILES = Toggle.m
 
 include $(THEOS_MAKE_PATH)/library.mk
+
+BUNDLE_NAME = FakeClockUpSettings
+FakeClockUpSettings_FILES = Preference.m
+FakeClockUpSettings_INSTALL_PATH = /Library/PreferenceBundles
+FakeClockUpSettings_FRAMEWORKS = UIKit CoreGraphics
+FakeClockUpSettings_PRIVATE_FRAMEWORKS = Preferences
+
+include $(THEOS_MAKE_PATH)/bundle.mk
+
+internal-stage::
+	$(ECHO_NOTHING)mkdir -p $(THEOS_STAGING_DIR)/Library/PreferenceLoader/Preferences$(ECHO_END)
+	$(ECHO_NOTHING)cp entry.plist $(THEOS_STAGING_DIR)/Library/PreferenceLoader/Preferences/FakeClockUp.plist$(ECHO_END)
---
+++ Preference.m
@@ -0,0 +1,100 @@
+#import <UIKit/UIKit.h>
+#import <Preferences/Preferences.h>
+
+__attribute__((visibility("hidden")))
+@interface LicenseController : PSViewController {
+  UITextView *view;
+}
+@end
+
+@implementation LicenseController
+
+- (id)initForContentSize:(CGSize)size
+{
+  if ([[PSViewController class] instancesRespondToSelector:@selector(initForContentSize:)])
+    self = [super initForContentSize:size];
+  else
+    self = [super init];
+  if (self) {
+    CGRect frame;
+    frame.origin = CGPointZero;
+    frame.size = size;
+    view = [[UITextView alloc] initWithFrame:frame];
+    NSData *data = [NSData dataWithContentsOfFile:@"/Library/PreferenceBundles/FakeClockUpSettings.bundle/LICENSE"];
+    view.text = [[NSString alloc] initWithData:data encoding:NSUTF8StringEncoding]; 
+    view.font = [UIFont systemFontOfSize:8.0f];
+  }
+  return self;
+}
+
+- (UIView *)view {
+	return view;
+}
+
+- (CGSize)contentSize {
+	return [view frame].size;
+}
+
+- (void)dealloc
+{
+	[view release];
+	[super dealloc];
+}
+@end
+
+__attribute__((visibility("hidden")))
+@interface FCPreferenceController : PSListController
+- (id)specifiers;
+@end
+
+@implementation FCPreferenceController
+
+- (id)specifiers
+{
+  if (_specifiers == nil) {
+    _specifiers = [[self loadSpecifiersFromPlistName:@"FakeClockUp" target:self] retain];
+  }
+  return _specifiers;
+}
+
+- (void)setDurationValue:(id)value specifier:(id)specifier
+{
+  [self setPreferenceValue:value specifier:specifier];
+  float duration = [value floatValue];
+  float sliderValue;
+  if (duration == 1.0f) {
+    sliderValue = 10;
+  } else if (duration < 1) {
+    sliderValue = (1.0 - duration) * 10.0 + 10;
+  } else {
+    sliderValue = 11.0f - duration;
+  }
+  PSSpecifier *sliderSpecifier = [self specifierForID:@"slider"];
+  [self setPreferenceValue:[NSNumber numberWithFloat:sliderValue] specifier:sliderSpecifier];
+  [self reloadSpecifier:sliderSpecifier];
+  [[NSUserDefaults standardUserDefaults] synchronize];
+  
+}
+
+- (void)setSliderValue:(id)value specifier:(id)specifier
+{
+  [self setPreferenceValue:value specifier:specifier];
+  NSLog(@"read slider = %f", [value floatValue]);
+  int sliderValue = [value intValue];
+  NSLog(@"int slider = %d", sliderValue);
+  double duration;
+  if (sliderValue == 10) {
+    duration = 1.0f;
+  } else if (sliderValue > 10) {
+    duration = 1.0L - (double)(sliderValue % 10) / 10.0L;
+  } else {
+    duration = 11.0f - (double)sliderValue;
+  }
+  PSSpecifier *durationSpecifier = [self specifierForID:@"duration"];
+  [self setPreferenceValue:[NSString stringWithFormat:@"%f", duration] specifier:durationSpecifier];
+  [self reloadSpecifier:durationSpecifier];
+  [[NSUserDefaults standardUserDefaults] synchronize];
+}
+
+@end
+
